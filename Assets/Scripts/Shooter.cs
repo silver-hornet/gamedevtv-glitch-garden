b587d7c9e5515dbe05aaea8c294e97e77d70c0d2
silver-hornet <70822422+silver-hornet@users.noreply.github.com>
2021-01-23 10:57:38 -0500

    Completed Lesson 153

--- Assets/Scripts/Shooter.cs
+++ Assets/Scripts/Shooter.cs
@@ -6,10 +6,53 @@ public class Shooter : MonoBehaviour
 {
     [SerializeField] GameObject projectile;
     [SerializeField] GameObject gun;
+    Animator animator;
+    AttackerSpawner myLaneSpawner;
+
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        SetLaneSpawner();
+    }
+
+    void Update()
+    {
+        if (IsAttackerInLane())
+        {
+            animator.SetBool("isAttacking", true);
+        }
+        else
+        {
+            animator.SetBool("isAttacking", false);
+        }
+    }
 
     public void Fire()
     // This is called from an Animation Event
     {
         Instantiate(projectile, gun.transform.position, gun.transform.rotation);
+    }
+
+    void SetLaneSpawner()
+    {
+        AttackerSpawner[] spawners = FindObjectsOfType<AttackerSpawner>();
+
+        foreach (AttackerSpawner spawner in spawners)
+        {
+            bool IsCloseEnough = (Mathf.Abs(spawner.transform.position.y - transform.position.y) <= Mathf.Epsilon);
+            // Using Mathf.Epsilon since the number will likely get very close to 0, but not exactly 0
+            if (IsCloseEnough)
+            {
+                myLaneSpawner = spawner;
+            }
+        }
+    }
+
+    bool IsAttackerInLane()
+    {
+        if (myLaneSpawner.transform.childCount <= 0)
+            return false;
+        else
+            return true;
     }
 }
