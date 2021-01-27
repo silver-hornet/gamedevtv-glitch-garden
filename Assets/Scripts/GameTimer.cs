3f1be1af52dc0b5eed5a539f003cc8bfacee79ec
silver-hornet <70822422+silver-hornet@users.noreply.github.com>
2021-01-27 08:29:05 -0500

    Completed Lesson 162

---
+++ Assets/Scripts/GameTimer.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameTimer : MonoBehaviour
+{
+    [Tooltip("Our level time in SECONDS")]
+    [SerializeField] float levelTime = 10f;
+
+    void Update()
+    {
+        GetComponent<Slider>().value = Time.timeSinceLevelLoad / levelTime;
+
+        bool timerFinished = (Time.timeSinceLevelLoad >= levelTime);
+        if (timerFinished)
+        {
+            Debug.Log("Level timer expired!");
+        }
+    }
+}
