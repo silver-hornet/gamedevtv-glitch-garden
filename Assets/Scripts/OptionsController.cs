c495accb975a4b7a95f5295b08cbbd4ffada7c80
silver-hornet <70822422+silver-hornet@users.noreply.github.com>
2021-01-29 07:56:25 -0500

    Completed Lesson 169

---
+++ Assets/Scripts/OptionsController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class OptionsController : MonoBehaviour
+{
+    [SerializeField] Slider volumeSlider;
+    [SerializeField] float defaultVolume = 0.8f;
+
+    void Start()
+    {
+        volumeSlider.value = PlayerPrefsController.GetMasterVolume();
+    }
+
+    void Update()
+    {
+        var musicPlayer = FindObjectOfType<MusicPlayer>();
+        if (musicPlayer)
+            musicPlayer.SetVolume(volumeSlider.value);
+        else
+            Debug.LogWarning("No music player found. Did you start from splash screen?");
+    }
+
+    public void SaveAndExit()
+    {
+        PlayerPrefsController.SetMasterVolume(volumeSlider.value);
+        FindObjectOfType<LevelLoad>().LoadMainMenu();
+    }
+
+    public void SetDefaults()
+    {
+        volumeSlider.value = defaultVolume;
+    }
+}
