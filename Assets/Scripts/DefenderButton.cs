709cd819fa191a781d182e03077c6c2ceee205dd
silver-hornet <70822422+silver-hornet@users.noreply.github.com>
2021-01-30 09:01:43 -0500

    Completed Lesson 174

--- Assets/Scripts/DefenderButton.cs
+++ Assets/Scripts/DefenderButton.cs
@@ -1,17 +1,32 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class DefenderButton : MonoBehaviour
 {
     [SerializeField] Defender defenderPrefab;
+
+    void Start()
+    {
+        LabelButtonWithCost();
+    }
+
+    void LabelButtonWithCost()
+    {
+        Text costText = GetComponentInChildren<Text>();
+        if (!costText)
+            Debug.LogError(name + " has no cost text. Add some!");
+        else
+            costText.text = defenderPrefab.GetStarCost().ToString();
+    }
 
     void OnMouseDown()
     {
         var buttons = FindObjectsOfType<DefenderButton>();
         foreach (DefenderButton button in buttons)
         {
-            button.GetComponent<SpriteRenderer>().color = new Color32(41, 41, 41, 255);
+            button.GetComponent<SpriteRenderer>().color = new Color32(85, 85, 85, 255);
         }
 
         GetComponent<SpriteRenderer>().color = Color.white;
